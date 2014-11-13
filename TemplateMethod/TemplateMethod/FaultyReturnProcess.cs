61921a0fb2957a491e88eb8488e4ab77a7ecbfba
zxj963577494 <963577494@qq.com>
2014-11-13 13:49:28 +0800

    模版方法模式

---
+++ TemplateMethod/TemplateMethod/FaultyReturnProcess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplateMethod
+{
+    /// <summary>
+    /// 缺陷商品退货处理
+    /// </summary>
+    public class FaultyReturnProcess : ReturnProcessTemplate
+    {
+
+        /// <summary>
+        /// 计算退货费用
+        /// </summary>
+        /// <param name="returnOrder"></param>
+        protected override void CalclateRefundFor(ReturnOrder returnOrder)
+        {
+            returnOrder.AmountToRefund = returnOrder.PricePaid + returnOrder.PostageCost; 
+        }
+
+        /// <summary>
+        /// 缺陷商品需要返厂修理
+        /// </summary>
+        /// <param name="returnOrder"></param>
+        protected override void GenerateReturnTransactionFor(ReturnOrder returnOrder)
+        {
+            Console.WriteLine("创建了一张返厂单");
+        }
+    }
+}
